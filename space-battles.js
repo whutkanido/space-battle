85d5bef826cf144777f5f8d432b9f90a9510166c
whutkanido <flagranthusk@gmail.com>
2021-08-08 21:37:34 -0400

    basic battle structure

--- space-battles.js
+++ space-battles.js
@@ -1,6 +1,6 @@
 // Generate a random whole number between min,max
-function randomIntBetween(min,max) {
-    return Math.floor(Math.random() * (max - min) + min + 1)
+function randomNumBetween(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 } 
 
 // Create objecs for the combatants
@@ -8,16 +8,18 @@ function randomIntBetween(min,max) {
 const mothership = {
     name: 'Mothership',
     hull: 20,
-    firepower: 2, // need function randomIntBetween(4,7)
-    accuracy: .7
+    firepower: 5,
+    accuracy: .8
 }
 
 const alien = {
     name: 'Alien Raider',
-    hull: 5, //between 3-6
-    firepower: 3, //between 2-4
-    accuracy: .6 //between .6 - .8
+    hull: randomNumBetween(8,9), 
+    firepower: randomNumBetween(2,4),
+    accuracy: (randomNumBetween(6,8) / 10)
 }
+
+
 
 // Player attack phase
 
@@ -25,12 +27,34 @@ const playerAttack = () =>{
     if(alien.hull > 0 && Math.random() < mothership.accuracy){
         console.log(mothership.name,'fires torpedos and HITS for',mothership.firepower,'damage.')
         alien.hull -= mothership.firepower
-        console.log(alien.name,'has',alien.hull,'HP remaining')
-    } else if (alien.hull <= 0){
+        } 
+    else if (alien.hull <= 0){
         console.log('The',alien.name,'has been ERADICATED!')
-    } else {
+        } 
+    else {
         console.log(mothership.name,'fires torpedos and MISSES')
     }
+}
+
+// Enemy attack phase
+
+const enemyAttack = () =>{
+    if(mothership.hull > 0 && Math.random() < alien.accuracy){
+        console.log(alien.name,'blasts the Mothership with toxic slime and does',alien.firepower,'damage.')
+        mothership.hull -= alien.firepower
+        } 
+    else if (mothership.hull <= 0){
+        console.log('The Mothership has been incinerated.  Game over')
+        } 
+    else {
+        console.log(alien.name,'tries to ram the Mothership and MISSES')
+    }
+}
+
+
+for(i = 0; i < 5; i++){
+playerAttack()
+enemyAttack()
 }
 
 
